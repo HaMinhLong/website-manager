fb96869ef918e834399027a3234a0fc60a6a8cb8
longhm <haminhlong3@gmail.com>
2022-11-23 11:16:16 +0700

    update scroll to view

--- src/layout/CreativeAgency/Header.tsx
+++ src/layout/CreativeAgency/Header.tsx
@@ -1,6 +1,6 @@
 // PROJECT IMPORT
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 // ICONS IMPORT
 import TwitterIcon from '@mui/icons-material/Twitter';
@@ -11,33 +11,57 @@ import SearchIcon from '@mui/icons-material/Search';
 import LogoPage from 'static/CreativeAgency/images/home/logo-page.png';
 
 const Header = () => {
+  const location = useLocation();
+
+  const handleClickScroll = (name: string) => {
+    const element = document.getElementById(name);
+    if (element) {
+      // 👇 Will scroll smoothly to the top of the next section
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  useEffect(() => {
+    if (location.hash) handleClickScroll(location.hash.split('#')[1]);
+    else handleClickScroll('home');
+  }, [location.pathname]);
+
   return (
-    <header className="header__home flex__center">
-      <div className="header__box container flex__between">
-        <div className="logo__box">
-          <Link to="/">
-            <img src={LogoPage} alt="logo page" />
-          </Link>
+    <>
+      <div id="home" style={{ overflow: 'hidden' }}></div>
+      <header className="header__home flex__center">
+        <div className="header__box container flex__between">
+          <div className="logo__box">
+            <Link to="/">
+              <img src={LogoPage} alt="logo page" />
+            </Link>
+          </div>
+          <ul className="navigation">
+            <li>
+              <Link to="/#home" onClick={() => handleClickScroll('home')}>
+                Home
+              </Link>
+              <Link to="/#service__home" onClick={() => handleClickScroll('service__home')}>
+                Services
+              </Link>
+              <Link to="/project">Projects</Link>
+              <Link to="/team">Team</Link>
+              <Link to="/#client" onClick={() => handleClickScroll('client')}>
+                Partners
+              </Link>
+              <Link to="/contact">Contacts</Link>
+              <Link to="/" className="icon">
+                <InstagramIcon />
+              </Link>
+              <Link to="/" className="icon">
+                <TwitterIcon />
+              </Link>
+              <SearchIcon />
+            </li>
+          </ul>
         </div>
-        <ul className="navigation">
-          <li>
-            <Link to="/">Home</Link>
-            <Link to="/#service__home">Services</Link>
-            <Link to="/project">Projects</Link>
-            <Link to="/team">Team</Link>
-            <Link to="/#client">Partners</Link>
-            <Link to="/contact">Contacts</Link>
-            <Link to="/" className="icon">
-              <InstagramIcon />
-            </Link>
-            <Link to="/" className="icon">
-              <TwitterIcon />
-            </Link>
-            <SearchIcon />
-          </li>
-        </ul>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
